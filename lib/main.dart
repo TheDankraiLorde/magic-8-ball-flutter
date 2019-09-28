8e456d8afdcb550f2b20263b3aa4d77fe5e2c439
TheDankraiLorde <menezesgaurav@gmail.com>
2019-09-28 17:36:56 +0530

    Finished Magic-8 Ball App

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -6,14 +8,43 @@ void main() => runApp(
       ),
     );
 
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballImageNumber = 1;
+
+  void randomise() {
+    setState(() {
+      ballImageNumber = (Random().nextInt(5) + 1);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: randomise,
+        child: Image.asset(
+          "images/ball$ballImageNumber.png",
+          width: 300.0,
+        ),
+      ),
+    );
+  }
+}
+
 class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue.shade900,
-        title: Text("Ask Me Anything"),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.blue.shade900,
+          title: Text("Ask Me Anything"),
+        ),
+        backgroundColor: Colors.blue,
+        body: Ball());
   }
 }
