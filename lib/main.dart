ff338ce1b60fa9fc6475f325c589da40a903d46a
TheDankraiLorde <menezesgaurav@gmail.com>
2019-09-28 15:18:41 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text("Ask Me Anything"),
+      ),
+    );
+  }
+}
